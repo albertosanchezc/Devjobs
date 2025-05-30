f311f4afbb03b76342991111c570471034207b4c
albertosanchezc <albertosanchezc98@gmail.com>
2025-05-29 19:17:43 -0600

    render a la vista show vacantes

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -3,7 +3,7 @@
     @forelse ($vacantes as $vacante)
         <div class="p-6 bg-white text-gray-900 border-b border-gray-200 md:flex md:justify-between items-center">
             <div class="space-y-3">
-                <a href="#" class="text-xl font-bold">
+                <a href="{{ route('vacantes.show', $vacante->id) }}" class="text-xl font-bold">
                     {{ $vacante->titulo }}
                 </a>
                 <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
